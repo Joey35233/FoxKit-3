18aeed0cd3a1f5368a8fe8da1d64d74d0da50d91
Joey35233 <joey35233@gmail.com>
2020-06-21 13:36:26 -0400

    Fixed gitignore.

---
+++ FoxKit/Assets/FoxKit/Config/Internal/FoxKitStartupState.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace FoxKit.Config.Internal
+{
+    public class FoxKitStartupState : ScriptableObject
+    {
+        private const string AssetPath = "Assets/FoxKit/FoxKitStartupState.asset";
+
+        [SerializeField]
+        public string ExportConfigPath;
+
+        public static FoxKitStartupState GetInstance()
+        {
+            FoxKitStartupState state = AssetDatabase.LoadAssetAtPath<FoxKitStartupState>(AssetPath);
+            if (state == null)
+            {
+                state = ScriptableObject.CreateInstance<FoxKitStartupState>();
+                AssetDatabase.CreateAsset(state, AssetPath);
+                AssetDatabase.SaveAssets();
+            }
+
+            return state;
+        }
+    }
+
+}
