610bbe517f28db83669130c4e0602a7115d3b38f
Joey35233 <joey35233@gmail.com>
2020-06-22 20:36:25 -0400

    Removed Workflow folder and began work on DataSet support.

---
+++ FoxKit/Assets/FoxKit/DataSet/DataSet.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+namespace FoxKit.DataSet
+{
+    class DataSet
+    {
+        public DataSet()
+        {
+
+        }
+
+        public DataSet(byte[] data)
+        {
+            Data = data;
+        }
+
+        private byte[] Data;
+    }
+}
