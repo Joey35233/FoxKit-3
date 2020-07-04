6b69d7c581fa5d7145f4c900275911f5e7598238
youarebritish <blue.oblivion@gmail.com>
2020-07-04 15:10:21 -0700

    Created inspector test entity/asset.

---
+++ FoxKit/Assets/FoxKit/Fox/DataSetAsset.cs
@@ -0,0 +1,24 @@
+﻿namespace Fox
+{
+    using System.Collections.Generic;
+    using UnityEngine;
+    using UnityEditor;
+
+    public class DataSetAsset : ScriptableObject
+    {
+        [SerializeReference]
+        public InspectorTestData test = new InspectorTestData();
+    
+        [MenuItem("FoxKit/Debug/FOX2/Create DataSet")]
+        public static void CreateMyAsset()
+        {
+            var asset = ScriptableObject.CreateInstance<DataSetAsset>() as DataSetAsset;
+
+            AssetDatabase.CreateAsset(asset, "Assets/delete/DataSet.asset");
+            AssetDatabase.SaveAssets();
+            EditorUtility.FocusProjectWindow();
+
+            Selection.activeObject = asset;
+        }
+    }
+}
