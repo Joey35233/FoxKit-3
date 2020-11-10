429c05974ac8c07b06a992173556b6baaeea7900
youarebritish <bishop@projectbc.net>
2020-11-10 15:34:07 -0800

    Update Int8Drawer.cs

--- FoxKit/Assets/FoxKit/Fox/Editor/Controls/Drawers/Int8Drawer.cs
+++ FoxKit/Assets/FoxKit/Fox/Editor/Controls/Drawers/Int8Drawer.cs
@@ -6,15 +6,25 @@ namespace Fox.Editor
     [CustomPropertyDrawer(typeof(System.SByte))]
     public class Int8Drawer : PropertyDrawer
     {
+        private SerializedProperty property;
+
         public override VisualElement CreatePropertyGUI(SerializedProperty property)
         {
+            this.property = property;
             var container = new VisualElement();
 
             var foldout = new Int8Field();
             foldout.label = property.name;
+            foldout.value = (sbyte)property.GetValue();
+            foldout.RegisterValueChangedCallback(OnValueChanged);
 
             container.Add(foldout);
             return container;
+        }
+
+        private void OnValueChanged(ChangeEvent<sbyte> evt)
+        {
+            this.property.SetValue(evt.newValue);
         }
     }
 }
