83c5f5cb01c0cab56b83fefdb1ae381fe7b99b8d
youarebritish <bishop@projectbc.net>
2020-11-10 15:14:51 -0800

    Added FilePtr drawer

---
+++ FoxKit/Assets/FoxKit/Fox/Editor/FilePtrDrawer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Fox.Editor
+{
+    [CustomPropertyDrawer(typeof(FilePtr<>))]
+    public class FilePtrDrawer : PropertyDrawer
+    {
+        private SerializedProperty fileProperty;
+
+        public override VisualElement CreatePropertyGUI(SerializedProperty property)
+        {
+            this.fileProperty = property.FindPropertyRelative("file");
+
+            var container = new VisualElement();
+            var uxmlTemplate = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/FoxKit/Fox/Editor/Controls/Drawers/FilePtrDrawer.uxml");
+            var drawer = uxmlTemplate.CloneTree(property.propertyPath);
+
+            var picker = drawer.Q<ObjectField>();
+            picker.value = this.fileProperty.GetValue() as UnityEngine.Object;
+            picker.label = property.name;
+            picker.allowSceneObjects = false;
+
+            // TODO: Filter this by asset type
+            picker.objectType = typeof(UnityEngine.Object);
+            picker.RegisterValueChangedCallback(OnValueChanged);
+
+            container.Add(drawer);
+            return container;
+        }
+
+        private void OnValueChanged(ChangeEvent<UnityEngine.Object> evt)
+        {
+            // TODO Figure out why undo doesn't appear instantly
+            Undo.RecordObject(this.fileProperty.serializedObject.targetObject, "FilePtr");
+            this.fileProperty.SetValue(evt.newValue);
+            this.fileProperty.serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
--- FoxKit/Assets/FoxKit/Fox/FilePtr.cs
+++ FoxKit/Assets/FoxKit/Fox/FilePtr.cs
@@ -8,33 +8,15 @@ namespace Fox
     /// </summary>
     /// <typeparam name="T">The type of the referenced file.</typeparam>
     [Serializable]
-    public struct FilePtr<T> : System.IEquatable<FilePtr<T>> where T : File
+    public class FilePtr<T> where T : File
     {
         /// <summary>
         /// The referenced file.
         /// </summary>
         [SerializeField]
-        private T file;
+        private UnityEngine.Object file;
 
-        public FilePtr(T file) => this.file = file;
-
-        public override bool Equals(object obj)
-        {
-            if (obj is null)
-            {
-                return false;
-            }
-
-            return this.Equals((FilePtr<T>)obj);
-        }
-
-        public bool Equals(FilePtr<T> other) => this.file == other.file;
-
-        public override int GetHashCode() => file.GetHashCode();
-
-        public static bool operator ==(FilePtr<T> lhs, FilePtr<T> rhs) => lhs.Equals(rhs);
-
-        public static bool operator !=(FilePtr<T> lhs, FilePtr<T> rhs) => !lhs.Equals(rhs);
+        public FilePtr(UnityEngine.Object file) => this.file = file;
 
         public override string ToString()
         {
