04e5306464dc5709b62076bea539e76ecc85c162
Bishop <blue.oblivion@gmail.com>
2022-05-12 14:04:50 -0700

    Added Locator gizmo

---
+++ FoxKit/Assets/FoxKit/Fox/Locator.cs
@@ -0,0 +1,15 @@
+﻿using Fox;
+using UnityEngine;
+
+namespace Fox.Core
+{
+    [System.Serializable]
+    public partial class Locator : TransformData
+    {
+        public override void InitializeGameObject(GameObject gameObject)
+        {
+            base.InitializeGameObject(gameObject);
+            gameObject.AddComponent<LocatorGizmo>();
+        }
+    }
+}
