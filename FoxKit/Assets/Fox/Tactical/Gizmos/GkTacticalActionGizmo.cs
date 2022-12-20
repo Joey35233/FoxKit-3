71ef3eedb3e61153b1cb70a27b72cd543d0453c3
kapuragu <38838430+kapuragu@users.noreply.github.com>
2022-12-20 17:17:18 +0700

    GkTacticalActionEdtior and Gizmo update

--- FoxKit/Assets/Fox/Tactical/Gizmos/GkTacticalActionGizmo.cs
+++ FoxKit/Assets/Fox/Tactical/Gizmos/GkTacticalActionGizmo.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Linq;
-using UnityEditor;
+﻿using UnityEditor;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 namespace Fox.Tactical
 {
@@ -13,15 +10,19 @@ namespace Fox.Tactical
     public class GkTacticalActionGizmo : MonoBehaviour
     {
         [HideInInspector]
+        public Vector3[] Waypoints;
+        [HideInInspector]
         public Color Color = Color.white;
         [HideInInspector]
         public Color ColorEdge = Color.yellow;
         [HideInInspector]
-        public Color ColorWaypoint = Color.green;
+        public Color ColorWaypoint = new Color(0.0f, 0.5f, 0.0f);
         [HideInInspector]
         public Vector3 Scale = Vector3.one*0.5f;
         public void DrawGizmos(bool isSelected)
         {
+            var rootPos = transform.position;
+
             //Get the entity
             var entityComponent = gameObject.GetComponent<Fox.Core.FoxEntity>();
             if (entityComponent == null)
@@ -37,40 +38,32 @@ namespace Fox.Tactical
             else
                 Gizmos.color = Color * 0.5f;
 
-            Gizmos.DrawWireCube(transform.position, Vector3.one * 0.25f);
+            Gizmos.DrawWireCube(rootPos, Vector3.one * 0.25f);
+
+            Waypoints = new Vector3[entity.waypoints.Count];
 
             //Draw waypoints and edges
             for (int i = 0; i < entity.waypoints.Count; i++)
             {
                 var waypoint = entity.waypoints[i].Get();
 
-                //but rotation???
-                var worldPosition = transform.position + waypoint.position;
-                if (isSelected)
-                {
-                    var newWorldPosition = Handles.PositionHandle(worldPosition, transform.rotation);
-                    waypoint.position = newWorldPosition - transform.position;
-                }
+                Waypoints[i] = transform.TransformPoint(new Vector3(-waypoint.position.x, waypoint.position.y, waypoint.position.z));
 
                 Gizmos.color = ColorWaypoint;
 
                 var right = Vector3.right * Scale.x;
-                Gizmos.DrawLine(worldPosition - right, worldPosition + right);
+                Gizmos.DrawLine(Waypoints[i] - right, Waypoints[i] + right);
 
                 var up = Vector3.up * Scale.y;
-                Gizmos.DrawLine(worldPosition - up, worldPosition + up);
+                Gizmos.DrawLine(Waypoints[i] - up, Waypoints[i] + up);
 
                 var forward = Vector3.forward * Scale.z;
-                Gizmos.DrawLine(worldPosition - forward, worldPosition + forward);
+                Gizmos.DrawLine(Waypoints[i] - forward, Waypoints[i] + forward);
 
                 if (i > 0)
                 {
-                    var prevWaypoint = entity.waypoints[i - 1].Get();
-
                     Gizmos.color = ColorEdge;
-                    var pointFrom = transform.position + prevWaypoint.position;
-                    var pointTo = transform.position + waypoint.position;
-                    Gizmos.DrawLine(pointFrom, pointTo);
+                    Gizmos.DrawLine(Waypoints[i - 1], Waypoints[i]);
                 }
             }
         }
@@ -81,6 +74,24 @@ namespace Fox.Tactical
         public void OnDrawGizmosSelected()
         {
             DrawGizmos(true);
+        }
+        public void Update()
+        {
+            //Get the entity
+            var entityComponent = gameObject.GetComponent<Fox.Core.FoxEntity>();
+            if (entityComponent == null)
+                return;
+
+            GkTacticalAction entity = entityComponent.Entity as GkTacticalAction;
+            if (entity == null)
+                return;
+
+            for (int i = 0; i < entity.waypoints.Count; i++)
+            {
+                var newLocalPosition = transform.InverseTransformPoint(Waypoints[i]);
+
+                entity.waypoints[i].Get().position = new Vector3(-newLocalPosition.x, newLocalPosition.y, newLocalPosition.z);
+            }
         }
     }
 }
