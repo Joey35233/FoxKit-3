741c6469a60d85f5dba81c75b58e8ac4e06e6c1b
kapuragu <38838430+kapuragu@users.noreply.github.com>
2022-12-25 00:39:57 +0700

    Start on GrxLightArrayFile reader

---
+++ FoxKit/Assets/FoxKit/MenuItems/ImportGrxLightArrayFile.cs
@@ -0,0 +1,22 @@
+﻿using Fox.Fio;
+using Fox.Grx;
+using UnityEditor;
+using CsSystem = System;
+
+namespace FoxKit.MenuItems
+{
+    public class ImportGrxLightArrayFile
+    {
+        [MenuItem("FoxKit/Import/GrxLightArray")]
+        private static void OnImportAsset()
+        {
+            var assetPath = EditorUtility.OpenFilePanel("Import GeoxPathFixedPack", "", "gpfp");
+            if (string.IsNullOrEmpty(assetPath))
+                return;
+            using var reader = new FileStreamReader(System.IO.File.OpenRead(assetPath));
+            var grxlaReader = new GrxLightArrayFileReader();
+            var scene = grxlaReader.Read(reader);
+            scene.name = CsSystem.IO.Path.GetFileNameWithoutExtension(assetPath);
+        }
+    }
+}
