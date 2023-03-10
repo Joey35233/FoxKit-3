0e14a9831a26fd0447241b9f9749633f9660628d
kapuragu <38838430+kapuragu@users.noreply.github.com>
2023-03-10 22:23:57 +0700

    41204288 made a vehicle edge event

--- FoxKit/Assets/Tpp/GameKit/Route/TppRouteUnknown41204288EdgeEvent.cs
+++ FoxKit/Assets/Tpp/GameKit/Route/TppRouteUnknown41204288EdgeEvent.cs
@@ -9,6 +9,13 @@ namespace Tpp.GameKit
 		public static readonly StrCode32 Id = HashingBitConverter.ToStrCode32(41204288);
 		public override StrCode32 GetId() => Id;
 
-		public static TppRouteUnknown41204288EdgeEvent Deserialize(FileStreamReader reader) => new TppRouteUnknown41204288EdgeEvent { binaryData = new StaticArray<uint>(new uint[] { reader.ReadUInt32(), reader.ReadUInt32(), reader.ReadUInt32(), reader.ReadUInt32() }), };
-	}
+		public static TppRouteUnknown41204288EdgeEvent Deserialize(FileStreamReader reader)
+        {
+            var result = new TppRouteUnknown41204288EdgeEvent { railId = new String(reader.ReadStrCode32().ToString()), rpm = reader.ReadUInt32() };
+
+            reader.SkipPadding(8);
+
+            return result;
+        }
+    }
 }
