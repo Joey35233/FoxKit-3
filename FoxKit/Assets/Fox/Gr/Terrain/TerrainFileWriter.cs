2b2920289527924cb445420da534d5129128198c
Bishop <blue.oblivion@gmail.com>
2023-06-29 17:13:58 -0700

    Slight start on tre2 writer

--- FoxKit/Assets/Fox/Gr/Terrain/TerrainFileWriter.cs
+++ FoxKit/Assets/Fox/Gr/Terrain/TerrainFileWriter.cs
@@ -1,10 +1,24 @@
+using Fox.Fio;
 using Fox.Gr;
+using System.Collections.Generic;
 using System.IO;
+
+enum FOX_DATA_NODE_ENTRY_PARAM_TYPE
+{
+    FOX_DATA_NODE_ENTRY_PARAM_TYPE_UINT = 0,
+	FOX_DATA_NODE_ENTRY_PARAM_TYPE_STRING = 1,
+	FOX_DATA_NODE_ENTRY_PARAM_TYPE_FLOAT = 2,
+}
 
 public class TerrainFileWriter
 {
     private BinaryWriter writer;
     private TerrainFileAsset terrain;
+
+    /// <summary>
+    /// Offsets at which to write string table offsets
+    /// </summary>
+    private readonly Dictionary<Fox.Kernel.String, long> stringLiterals = new Dictionary<Fox.Kernel.String, long>();
 
     internal void Write(BinaryWriter binWriter, TerrainFileAsset terrain)
     {
@@ -12,6 +26,10 @@ public class TerrainFileWriter
         this.terrain = terrain;
 
         WriteHeader();
+        _ = writer.Seek(32, SeekOrigin.Begin);
+        WriteNodes();
+
+        // TODO Write strings
     }
 
     private void WriteHeader()
@@ -19,8 +37,23 @@ public class TerrainFileWriter
         writer.Write((int)4);
         writer.Write((int)32);
         writer.Write((int)1049512);
-        writer.Write(1840366096);
-        writer.Write(1049268);
+        WriteFoxDataString(new Fox.Kernel.String("tre2"));
         writer.Write(0);
+    }
+
+    private void WriteNodes()
+    {
+    }
+
+    private void WriteNode(Fox.Kernel.String name)
+    {
+        WriteFoxDataString(name);
+    }
+
+    private void WriteFoxDataString(Fox.Kernel.String str)
+    {
+        writer.WriteStrCode32(str.Hash32);
+        stringLiterals.Add(str, writer.BaseStream.Position);
+        writer.Write(-1);
     }
 }
