061b89b723b577bd7286da8af6eeafc8d5c3614f
josephclampett-education <111520419+josephclampett-education@users.noreply.github.com>
2023-12-14 20:02:36 -0500

    Stub FRDV reader

---
+++ FoxKit/Assets/Fox/Animx/RigDriver/HelpBoneFile.cs
@@ -0,0 +1,55 @@
+﻿using Fox.Fio;
+using System;
+using System.Runtime.InteropServices;
+using UnityEditor;
+using UnityEngine;
+using String = Fox.Kernel.String;
+
+namespace Fox.Animx
+{
+    [StructLayout(LayoutKind.Sequential, Pack = 16)]
+    unsafe struct FrdvHeader
+    {
+        public fixed char Signature[4];
+
+        public uint Version;
+
+        public uint EntryCount;
+    };
+
+    public partial class HelpBoneFile : Fox.Core.RawFile
+    {
+        public void Read(ReadOnlySpan<byte> data)
+        {
+            if (Selection.transforms.Length < 1)
+            {
+                return;
+            }
+            else if (Selection.transforms.Length > 1)
+            {
+                Debug.LogWarning($"Multiple objects selected. Applying RigDrivers to first: {Selection.transforms[0].name}.");
+            }
+
+            UnityEngine.Transform target = Selection.transforms[0];
+
+            unsafe
+            {
+                fixed (byte* dataPtr = data)
+                {
+                    var header = (FrdvHeader*)dataPtr;
+                    Debug.Assert(header->Version == 2830171915);
+
+                    uint* offsets = (uint*)(dataPtr + sizeof(FrdvHeader));
+
+                    for (uint i = 0; i < header->EntryCount; i++)
+                    {
+                        var unit = (DriverUnit*)(dataPtr + offsets[i]);
+
+                        var driver = RigDriver.Deserialize(unit);
+                        driver.name = $"RigDriver{i:D4}";
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ FoxKit/Assets/Fox/Animx/RigDriver/RigDriver.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+namespace Fox.Animx
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public struct DriverUnit
+    {
+
+    }
+
+    public class RigDriver : MonoBehaviour
+    {
+        public static unsafe RigDriver Deserialize(DriverUnit* unit)
+        {
+            return null;
+        }
+    }
+}
