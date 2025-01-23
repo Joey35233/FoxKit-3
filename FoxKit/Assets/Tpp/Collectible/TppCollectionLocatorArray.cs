d307d1892ffe3b8829503919dcc525e24dae3695
kapuragu <caplagcreep@yahoo.com>
2025-01-24 03:54:06 +0700

    Update TppCollectionLocatorArray.cs

--- FoxKit/Assets/Tpp/Collectible/TppCollectionLocatorArray.cs
+++ FoxKit/Assets/Tpp/Collectible/TppCollectionLocatorArray.cs
@@ -2,6 +2,9 @@
 
 using Fox.Core;
 using Fox.Core.Utils;
+using System;
+using System.Globalization;
+using System.Linq;
 using UnityEngine;
 using CsSystem = System;
 
@@ -85,23 +88,87 @@ namespace Tpp.Collectible
             TYPE_HERB = 1,
             TYPE_STATION = 2,
         }
+        private FilePtr GetModelFilePath(TppCollection_Type type)
+        {
+            string pathString = "";
+            switch (type)
+            {
+                case TppCollection_Type.TYPE_DIAMOND_SMALL:
+                case TppCollection_Type.TYPE_DIAMOND_LARGE:
+                    pathString = "/Assets/tpp/item/dia/Scenes/dia1_main0_def.fmdl";
+                    break;
+                case TppCollection_Type.TYPE_CASSETTE:
+                    pathString = "/Assets/tpp/item/cct/Scenes/cct0_main1_def.fmdl";
+                    break;
+                case TppCollection_Type.TYPE_DEVELOPMENT_FILE:
+                case TppCollection_Type.TYPE_EMBLEM:
+                    pathString = "/Assets/tpp/item/ibo/Scenes/ibo6_main0_def.fmdl";
+                    break;
+                case TppCollection_Type.TYPE_SHIPPING_LABEL:
+                    pathString = "/Assets/tpp/environ/object/middle_africa/pallet/mafr_pllt002/scenes/mafr_pllt002_vrtn004.fmdl";
+                    break;
+                default:
+                    if (type >= TppCollection_Type.TYPE_MATERIAL_CM_0 && type <= TppCollection_Type.TYPE_MATERIAL_BR_7)
+                        pathString = "/Assets/tpp/item/ibo/Scenes/ibo3_main0_def.fmdl";
+                    break;
+                case TppCollection_Type.TYPE_POSTER_SOL_AFGN:
+                case TppCollection_Type.TYPE_POSTER_SOL_MAFR:
+                case TppCollection_Type.TYPE_POSTER_SOL_ZRS:
+                case TppCollection_Type.TYPE_POSTER_GRAVURE_V:
+                case TppCollection_Type.TYPE_POSTER_MOE_V:
+                    pathString = "/Assets/tpp/item/cbx/Scenes/cbx0_post1_def.fmdl";
+                    break;
+                case TppCollection_Type.TYPE_POSTER_GRAVURE_H:
+                case TppCollection_Type.TYPE_POSTER_MOE_H:
+                    pathString = "/Assets/tpp/item/cbx/Scenes/cbx0_post0_def.fmdl";
+                    break;
+                case TppCollection_Type.TYPE_PICTURE_PAZ:
+                    pathString = "/Assets/tpp/item/pik/Scenes/pik6_main1_sta.fmdl";
+                    break;
+                case TppCollection_Type.TYPE_HERB_G_CRESCENT:
+                    pathString = "/Assets/tpp/environ/object/afghanistan/flower/afgh_flwr005/scenes/afgh_flwr005.fmdl";
+                    break;
+                case TppCollection_Type.TYPE_HERB_A_PEACH:
+                    pathString = "/Assets/tpp/environ/object/middle_africa/flower/mafr_flwr002/scenes/mafr_flwr002.fmdl";
+                    break;
+                case TppCollection_Type.TYPE_HERB_DIGITALIS_P:
+                    pathString = "/Assets/tpp/environ/object/middle_africa/flower/mafr_flwr001/scenes/mafr_flwr001.fmdl";
+                    break;
+                case TppCollection_Type.TYPE_HERB_DIGITALIS_R:
+                    pathString = "/Assets/tpp/environ/object/middle_africa/flower/mafr_flwr001/scenes/mafr_flwr001_vrtn002.fmdl";
+                    break;
+                case TppCollection_Type.TYPE_HERB_B_CARROT:
+                    pathString = "/Assets/tpp/environ/object/afghanistan/flower/afgh_flwr004/scenes/afgh_flwr004.fmdl";
+                    break;
+                case TppCollection_Type.TYPE_HERB_WORM_WOOD:
+                    pathString = "/Assets/tpp/environ/object/afghanistan/flower/afgh_flwr001/scenes/afgh_flwr001.fmdl";
+                    break;
+                case TppCollection_Type.TYPE_HERB_TARRAGON:
+                    pathString = "/Assets/tpp/environ/object/afghanistan/flower/afgh_flwr003/scenes/afgh_flwr003.fmdl";
+                    break;
+                case TppCollection_Type.TYPE_HERB_HAOMA:
+                    pathString = "/Assets/tpp/environ/object/afghanistan/flower/afgh_flwr002/scenes/afgh_flwr002.fmdl";
+                    break;
+                case TppCollection_Type.TYPE_STATION:
+                    break;
+            };
+            return new FilePtr(new Fox.Path(pathString));
+        }
         private Quaternion DecompressQuaternion( uint retQuat )
         {
             float x = (float)((retQuat >> 10 & 0x3ff) * 0.0009775171);
             float y = (float)((retQuat & 0x3ff) * 0.0009775171);
             float z = (float)(1.0 - x) - y;
+
             if ((retQuat >> 0x1d & 1) != 0)
-            {
                 x = -x;
-            }
+
             if (((byte)(retQuat >> 0x1d) & 2) != 0)
-            {
                 y = -y;
-            }
+
             if ((retQuat >> 0x1d & 4) != 0)
-            {
                 z = -z;
-            }
+
             float theta = (float)((retQuat >> 0x14 & 0x1ff) * 0.001956947 * 3.141593 * 0.5);
             float sqrted = (float)1.0 / Mathf.Sqrt(y * y + x * x + z * z);
             float sinfTheta = Mathf.Sin(theta);
@@ -110,12 +177,40 @@ namespace Tpp.Collectible
         }
         private uint CompressQuaternion(Quaternion rotQuat)
         {
-            uint compress = 0;
+            float x = rotQuat.x; float y = rotQuat.y;
+            float z = rotQuat.z; float w = rotQuat.w;
 
-            float theta = Mathf.Acos(rotQuat.w);
-            float sinfTheta = Mathf.Asin(rotQuat.w);
+            if(w<0)
+            {
+                x=-x; 
+                y=-y;
+                z=-z;
+                w=-w;
+            }
+            byte signs=0;
+            if (x < 0 )
+                signs = 1; x = -x;
 
-            return compress;
+            if (y < 0)
+                signs |= 2; y = -y;
+
+            if (z < 0)
+                signs |= 4; z = -z;
+
+            float normFactor = 1.0f / (x + y + z);
+            x *= normFactor;
+            y *= normFactor;
+            z *= normFactor;
+
+            float wCompressed = Mathf.Acos(w) * (2.0f * 0.3183099f * 511.0f) + 0.5f;
+            float xCompressed = x * 1023.0f + 0.5f;
+            float yCompressed = y * 1023.0f + 0.5f;
+                                                    
+            int wInt = Mathf.FloorToInt(wCompressed);
+            int xInt = Mathf.FloorToInt(xCompressed);
+            int yInt = Mathf.FloorToInt(yCompressed);
+
+            return (uint)((yInt & 0x3FF) | ((xInt & 0x3FF) << 10) | ((wInt & 0x3FF) << 20) | (signs << 29));
         }
         private TppCollection_GroupType GetGroupId(TppCollection_Type type)
         {
@@ -127,16 +222,15 @@ namespace Tpp.Collectible
             else
                 return TppCollection_GroupType.TYPE_STATION;
         }
-        private const ushort NUM_BLOCKS = 32;
-        private const ushort METERS_PER_BLOCK = 128 / 1;
-        private static ushort PositionFWSToBlockIndex(Vector3 positionFWS)
+        private const ushort NUM_BLOCKS = 64;
+        private static ushort GetSegmentIndex(Vector3 position)
         {
-            // block indices [0,32) x [0,32)
-            ushort blockX = (ushort)Mathf.FloorToInt((METERS_PER_BLOCK * NUM_BLOCKS / 2 + positionFWS.x) / METERS_PER_BLOCK);
-            ushort blockZ = (ushort)Mathf.FloorToInt((METERS_PER_BLOCK * NUM_BLOCKS / 2 + positionFWS.z) / METERS_PER_BLOCK);
+            int blockIdx = Mathf.FloorToInt(position.x / NUM_BLOCKS);
+            float blockX = Mathf.Clamp(blockIdx + NUM_BLOCKS, 0, NUM_BLOCKS * 2 - 1);
+            int blockIdz = Mathf.FloorToInt(position.z / NUM_BLOCKS);
+            float blockZ = Mathf.Clamp(blockIdz + NUM_BLOCKS, 0, NUM_BLOCKS * 2 - 1);
 
-            // one-value block index [0, 32*32)
-            return (ushort)(blockZ * NUM_BLOCKS + blockX);
+            return (ushort)(blockZ * 0x80 + blockX);
         }
         public override void OnDeserializeEntity(GameObject gameObject, TaskLogger logger)
         {
@@ -157,6 +251,7 @@ namespace Tpp.Collectible
 
                 TppCollectionLocator colLocator = new GameObject(uniqueId).AddComponent<TppCollectionLocator>();
 
+                FilePtr modelFile = GetModelFilePath(typeId);
                 //TODO load relevant model
                 GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
                 cube.transform.parent = colLocator.transform;
@@ -184,9 +279,10 @@ namespace Tpp.Collectible
         }
         public void Override(EntityExportContext context)
         {
-            TppCollectionLocator[] locators = this.GetComponentsInChildren<TppCollectionLocator>();
+            var locators = this.GetComponentsInChildren<TppCollectionLocator>().ToList();
+            locators.OrderBy(x => x.type).ThenBy(x=>GetSegmentIndex(Fox.Math.UnityToFoxVector3(x.transform.position)));
 
-            int locatorsCount = locators.Length;
+            int locatorsCount = locators.Count;
 
             CsSystem.Collections.Generic.List<Vector3> _positions = new(locatorsCount);
             CsSystem.Collections.Generic.List<uint> _rotations = new(locatorsCount);
@@ -196,17 +292,17 @@ namespace Tpp.Collectible
             CsSystem.Collections.Generic.List<ushort> _locatorIndices = new();
             CsSystem.Collections.Generic.List<ushort> _locatorCounts = new();
 
-            CsSystem.Collections.Generic.List<ushort> _groupIds = new();
+            CsSystem.Collections.Generic.List<byte> _groupIds = new();
             CsSystem.Collections.Generic.List<ushort> _segmentInfoIndices = new();
             CsSystem.Collections.Generic.List<ushort> _segmentInfoCounts = new();
 
+            int locatorGroupIndex = 0;
             foreach (TppCollection_GroupType groupType in CsSystem.Enum.GetValues(typeof(TppCollection_GroupType)))
             {
                 CsSystem.Collections.Generic.List<ushort> groupSegmentIndices = new();
                 CsSystem.Collections.Generic.List<ushort> groupLocatorIndices = new();
                 CsSystem.Collections.Generic.List<ushort> groupLocatorCounts = new();
-                int locatorGroupIndex = 0;
-                for (int i = 0; i < locators.Length; i++)
+                for (int i = 0; i < locators.Count; i++)
                 {
                     if (groupType == GetGroupId(locators[i].type))
                     {
@@ -222,14 +318,37 @@ namespace Tpp.Collectible
                         //  Info
                         TppCollection_Type type = locators[i].type;
                         string name = locators[i].name;
-                        if (!uint.TryParse(name, out uint nameHash))
+                        if (!uint.TryParse(name, NumberStyles.HexNumber, null, out uint nameHash))
                             nameHash = (uint)new Fox.StrCode32(name).GetHashCode();
-                        _infos.Insert(locatorGroupIndex, (nameHash & 0xFFFFFF) | ((uint)type << 24));
+                        uint infos = (nameHash & 0xFFFFFF) | ((uint)type << 24);
+                        _infos.Insert(locatorGroupIndex, infos);
 
                         //Segment index
-                        ushort segmentInfo = PositionFWSToBlockIndex(position);
+                        ushort segmentIndex = GetSegmentIndex(Fox.Math.UnityToFoxVector3(locators[i].transform.position));
+
+                        if (!groupSegmentIndices.Contains(segmentIndex))
+                        {
+                            groupSegmentIndices.Add(segmentIndex);
+                            ushort segmentInfoIndex = (ushort)groupSegmentIndices.IndexOf(segmentIndex);
+                            groupLocatorIndices.Add((ushort)locatorGroupIndex);
+                            groupLocatorCounts.Insert(segmentInfoIndex,0);
+                            if (!_groupIds.Contains((byte)groupType))
+                            {
+                                _groupIds.Add((byte)groupType);
+                                ushort groupIndex = (ushort)_groupIds.IndexOf((byte)groupType);
+                                _segmentInfoIndices.Add((ushort)groupSegmentIndices.IndexOf(segmentIndex));
+                                _segmentInfoCounts.Insert(groupIndex, 0);
+                            }
+                            _segmentInfoCounts[_groupIds.IndexOf((byte)groupType)]++;
+                        }
+                        groupLocatorCounts[groupSegmentIndices.IndexOf(segmentIndex)]++;
+
+                        locatorGroupIndex++;
                     }
                 }
+                _segmentIndices.AddRange(groupSegmentIndices);
+                _locatorIndices.AddRange(groupLocatorIndices);
+                _locatorCounts.AddRange(groupLocatorCounts);
             }
 
             context.OverrideProperty(nameof(positions), _positions);
