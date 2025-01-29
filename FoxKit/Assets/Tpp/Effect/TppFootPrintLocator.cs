2f224f6e3b5529b8049e095c507e57b8320e80bb
kapuragu <caplagcreep@yahoo.com>
2025-01-30 00:10:26 +0700

    TppFootPrint

---
+++ FoxKit/Assets/Tpp/Effect/TppFootPrintLocator.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using static Tpp.Effect.TppFootPrint;
+
+namespace Tpp.Effect
+{
+    [UnityEditor.InitializeOnLoad, UnityEngine.AddComponentMenu("TppEffect/TppFootPrintLocator")]
+    public class TppFootPrintLocator : MonoBehaviour
+    {
+        [field: SerializeField]
+        public FootPrintChara footChara = FootPrintChara.BOOT;
+        [field: SerializeField]
+        public bool nvg = true;
+        [field: SerializeField]
+        public bool footLR = false;
+        [field: SerializeField]
+        public float alpha = 1.0f;
+        [field: SerializeField]
+        public bool blood = false;
+        [field: SerializeField]
+        public bool enable = true;
+        public void OnDrawGizmos()
+        {
+            Gizmos.matrix = transform.localToWorldMatrix;
+
+            Gizmos.DrawCube(Vector3.forward*0.05f, new Vector3(0.1f, 0, 0.2f));
+        }
+        public void Reset()
+        {
+            transform.localScale = new Vector3(2.3f,1.1f,2.3f);
+        }
+    }
+}
