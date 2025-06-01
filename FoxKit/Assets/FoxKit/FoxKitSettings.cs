b65e2b8b6324f4efce9a862563e6790d639a05ec
Joey35233 <13488195+Joey35233@users.noreply.github.com>
2025-06-01 11:46:27 -0400

    Revert "Merge pull request #17 from Joey35233/terrain-dev"
    This reverts commit 61d4e2bc1eeae28de94d9cef8bbd1b7c805a224e, reversing
    changes made to f7388945397249731da26cc2d05cd14658aa2d52.

--- FoxKit/Assets/FoxKit/FoxKitSettings.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "FoxKitSettings", menuName = "FoxKit/Settings")]
-public class FoxKitSettings : ScriptableObject
-{
-    public string sourceAssetsPath;
-}
